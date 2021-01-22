1cf3b97e9e69a2f3ecc53b92e1735826f821302c
Alexander Kabui <alexanderkabua@gmail.com>
2021-01-22 21:09:08 +0300

    add sockets and templates

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
 import json
 import io
+from flask import Flask,render_template
+from flask_socketio import SocketIO,emit
+from time import sleep
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 obj = {"name":"Alexander","value":"Y"}
+
+def string_io_converter():
+	length = len(json.dumps(obj))
+	string_stream = io.StringIO(json.dumps(obj))
+	return string_stream,length
+
+
+@socketio.on('messenger')
+def test_connect():
+
+	string_stream,length = string_io_converter()
+
+	while length>string_stream.tell():
+		data = string_stream.read(1)
+		print(data)
+		emit("response",{'data':data})
+		sleep(0.1)
+
+
+@socketio.on("gemma")
+def gemma():
+	print("my name is alexis")
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
 results = json.dumps(obj).encode("utf-8")
 byte_results = io.BytesIO(results)
 counter = 0 
@@ -26,3 +64,5 @@ string_counter = 0
 while y > string_counter:
 	print(string_stream.read(1))
 	string_counter+=1
+
+
